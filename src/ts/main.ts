f03f45ad4b7a6cc36e1ff63eb17216943b5cf693
Vladislav <vladislav.malchevskiy.work@gmail.com>
2025-07-14 18:37:37 +0300

    init

---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+"use strict";
+import "../styles/main.scss"; // путь может отличаться
+// import form from "./modules/form";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // form();
+});
